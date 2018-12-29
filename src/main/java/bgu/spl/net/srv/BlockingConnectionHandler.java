a86e932604e4cbc0f67066f9088bbb44da9294c9
Alex Abramov <alexabramov2610@gmail.com>
2018-12-29 22:48:16 +0200

    aa

--- src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
+++ src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
@@ -63,14 +63,11 @@ public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler
 
     @Override
     public void send(bidiMessages.bidiMessage msg) {
-        // TODO: check if getString string is good
-        byte[] encodedData = msg.getString().getBytes();
-        // TODO: c++ msg sending
-        /*
-            encodes msg.getMsgToSend() string  to bytes and sends to client (to c++ app)
-         */
-
-        System.out.println(Thread.currentThread().getName() +" received : " + msg.getString() );
-
+        try {
+            out.write(encdec.encode(msg));
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
